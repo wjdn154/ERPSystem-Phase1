20e54982c401ad6ea58c7eda4358d092bfdc9844
허건호 <gj1563@naver.com>
2024-06-10 11:13:01 +0900

    # [제목] # <유형>: <설명> (최대 50자, 변경사항이 무엇인지 명확히 작성, 끝에 마침표 금지, 제목 작성 뒤 한줄 공백(제목과 본문 사이 간격을 주기 위함)) docs: 라이브러리 설명 추가
    # [본문]
    # 구체적인 내용 작성(여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내))
    - 라이브러리 상세 설명 추가.
    - 불필요한 출력 제거.
    # [유형]
    # feature: 새 기능
    # fix: 버그 수정
    # docs: 문서 변경
    # style: 코드 의미에 영향을 주지 않는 변경사항
    # refactor: 버그를 수정하거나 기능을 추가하지 않는 코드 변경
    #
    # -------------- 예시 --------------
    # docs: 커밋 메시지 템플릿 파일 추가
    #
    # 이 커밋은 프로젝트에 커밋 메시지 템플릿 파일 (gitmessage.txt)을 추가합니다.
    # 이 파일은 커밋 메시지 작성 시 일관성을 유지하기 위해 사용됩니다.
    # - 파일 경로: 프로젝트 루트 디렉토리
    # - 템플릿 파일은 커밋 메시지 작성 시 자동으로 불러와집니다.
    # - 일관된 커밋 메시지 형식을 유지할 수 있습니다.
    #

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
 }
 
-group = "org.example"
+group = "org.ERPSystem"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,8 +10,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.poi:poi:5.0.0") // POI 라이브러리 : 마이크로소프트 오피스 파일을 자바 언어로 읽고 쓰는 기능을 제공
+    implementation("org.apache.poi:poi-ooxml:5.0.0") // POI 라이브러리 : 마이크로소프트 오피스 파일을 자바 언어로 읽고 쓰는 기능을 제공
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+
 }
 
 tasks.getByName<Test>("test") {
