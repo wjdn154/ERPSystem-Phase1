b0de6a4f324987ead501363c09e7685020665e86
wjdn154 <wjdn154@gmail.com>
2024-06-19 02:22:34 +0900

    style: 주석 추가
    주석 추가

--- src/main/java/com/erp/system/Main.java
+++ src/main/java/com/erp/system/Main.java
@@ -2,11 +2,16 @@ package com.erp.system;
 
 
 import com.erp.system.common.DependencyInjector;
+import com.erp.system.financial.controller.basic_information_management.AccountInformationController;
 import com.erp.system.financial.controller.basic_information_management.PurchaseSalesSlipController;
 import com.erp.system.common.ERPDataInitializer;
 import com.erp.system.financial.controller.basic_information_management.TaxInvoiceController;
 import com.erp.system.financial.controller.book_keeping.AccountingLedgerController;
+import com.erp.system.financial.model.basic_information_management.account_information.BankAccount;
 import com.erp.system.financial.model.basic_information_management.purchase_sales_slip.Entry;
+import com.erp.system.financial.repository.basic_information_management.account_information.BankAccountRepository;
+
+import java.util.Optional;
 
 import static com.erp.system.common.PrintAllEntities.printAllEntities;
 
@@ -23,25 +28,51 @@ public class Main {
         PurchaseSalesSlipController purchaseSalesSlipController = injector.getInstance(PurchaseSalesSlipController.class);
         AccountingLedgerController accountingLedgerController = injector.getInstance(AccountingLedgerController.class);
         TaxInvoiceController taxInvoiceController = injector.getInstance(TaxInvoiceController.class);
+        AccountInformationController accountInformationController = injector.getInstance(AccountInformationController.class);
 
         printAllEntities(purchaseSalesSlipController.findAllEntries(), "code"); // Entry 출력
         printAllEntities(purchaseSalesSlipController.findAllVatTypes(), "code"); // VatType 출력
         printAllEntities(accountingLedgerController.findAllCashBooks(), "code"); // CashBook 출력
         printAllEntities(taxInvoiceController.findAllTaxInvoices(), "code"); // TaxInvoice 출력
 
-        taxInvoiceController.findAllTaxInvoices().forEach(taxInvoice -> {
-            System.out.println(taxInvoice.getNtsTransmissionStatus().getClass()); // enum 확인
-        });
 
-        Entry entry = new Entry.Builder("1")
+        // 테스트 객체 생성
+        BankAccount BA = new BankAccount.Builder()
+                .code("1")
+                .bankName("original")
+                .branchLocation("this.branchLocation")
+                .number("this.number")
+                .owner("this.owner")
+                .depositType("this.depositType")
+                .build();
+        BankAccount BA2 = new BankAccount.Builder()
+                .code("2")
+                .bankName("original")
+                .branchLocation("this.branchLocation")
+                .number("this.number")
+                .owner("this.owner")
+                .depositType("this.depositType")
                 .build();
 
-        System.out.println("entry.getName() = " + entry.getName());
+        // 저장
+        accountInformationController.saveBankAccount(BA); 
+        accountInformationController.saveBankAccount(BA2);
 
+        // 수정 전 조회
+        printAllEntities(accountInformationController.findAllBankAccounts(), "code");
+        
+        BankAccountRepository bankAccountRepository = injector.getInstance(BankAccountRepository.class);
+        
+        // 객체 수정
+        BankAccount bankAccount = bankAccountRepository.findByCode("2").get();
+        bankAccount.toBuilder().bankName("바꿧어요~~").build();
 
-        System.out.println("entry.getName() = " + entry.getName());
+        System.out.println("bankAccount = " + bankAccount.getBank_name());
 
-
-
+        // 수정된 객체 업데이트
+        bankAccountRepository.update(bankAccount);
+        
+        // 수정 후 조회
+        printAllEntities(accountInformationController.findAllBankAccounts(), "code");
     }
 }
--- src/main/java/com/erp/system/financial/model/basic_information_management/account_information/BankAccount.java
+++ src/main/java/com/erp/system/financial/model/basic_information_management/account_information/BankAccount.java
@@ -1,5 +1,9 @@
 package com.erp.system.financial.model.basic_information_management.account_information;
 
+import com.erp.system.common.NotNullValidator;
+import com.erp.system.common.annotation.NotNull;
+
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.UUID;
 
@@ -11,41 +15,48 @@ public class BankAccount {
     private final String id; // 계좌의 고유 식별자
     private final String code; // 계좌 코드
 
-    private Date opening_date; // 개설일
-    private String bank_name; // 은행명
-    private String branch_location; // 계좌계설지점
-    private String number; // 계좌번호
-    private String owner; // 계좌 소유자
-    private String deposit_type; // 예금종류
+    private  LocalDate openingDate; // 개설일
+    private  String bankName; // 은행명
+    private  String branchLocation; // 계좌계설지점
+    private  String number; // 계좌번호
+    private  String owner; // 계좌 소유자
+    private  String depositType; // 예금종류
+
+    public static int id_index = 1;
 
     public static class Builder {
-        private final String id;
-        private final String code;
+        private String id;
+        private String code;
 
-        private Date opening_date;
-        private String bank_name;
-        private String branch_location;
+        private LocalDate openingDate;
+        private String bankName;
+        private String branchLocation;
         private String number;
         private String owner;
-        private String deposit_type;
+        private String depositType;
 
-        public Builder(String code) {
-            this.id = UUID.randomUUID().toString();
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder code(String code) {
             this.code = code;
-        }
-
-        public Builder opening_date(Date opening_date) {
-            this.opening_date = opening_date;
             return this;
         }
 
-        public Builder bank_name(String bank_name) {
-            this.bank_name = bank_name;
+        public Builder openingDate(LocalDate opening_date) {
+            this.openingDate = opening_date;
             return this;
         }
 
-        public Builder branch_location(String branch_location) {
-            this.branch_location = branch_location;
+        public Builder bankName(String bank_name) {
+            this.bankName = bank_name;
+            return this;
+        }
+
+        public Builder branchLocation(String branch_location) {
+            this.branchLocation = branch_location;
             return this;
         }
 
@@ -59,25 +70,46 @@ public class BankAccount {
             return this;
         }
 
-        public Builder deposit_type(String deposit_type) {
-            this.deposit_type = deposit_type;
+        public Builder depositType(String deposit_type) {
+            this.depositType = deposit_type;
             return this;
         }
 
         public BankAccount build() {
             return new BankAccount(this);
         }
+
+        public BankAccount.Builder update(BankAccount test) {
+            return this;
+        }
     }// end of Builder
 
     private BankAccount(Builder builder) {
-        this.id = builder.id;
+        if(builder.id != null) {
+            this.id = builder.id;
+        }
+        else {
+            this.id = Integer.toString(id_index++);
+        }
         this.code = builder.code;
-        this.opening_date = builder.opening_date;
-        this.bank_name = builder.bank_name;
-        this.branch_location = builder.branch_location;
+        this.openingDate = builder.openingDate;
+        this.bankName = builder.bankName;
+        this.branchLocation = builder.branchLocation;
         this.number = builder.number;
         this.owner = builder.owner;
-        this.deposit_type = builder.deposit_type;
+        this.depositType = builder.depositType;
+    }
+
+    public Builder toBuilder() {
+        return new Builder()
+                .id(this.id)
+                .code(this.code)
+                .bankName(this.bankName)
+                .branchLocation(this.branchLocation)
+                .openingDate(this.openingDate)
+                .number(this.number)
+                .owner(this.owner)
+                .depositType(this.depositType);
     }
 
     public String getId() {
@@ -88,16 +120,16 @@ public class BankAccount {
         return code;
     }
 
-    public Date getOpening_date() {
-        return opening_date;
+    public LocalDate getOpening_date() {
+        return openingDate;
     }
 
     public String getBank_name() {
-        return bank_name;
+        return bankName;
     }
 
     public String getBranch_location() {
-        return branch_location;
+        return branchLocation;
     }
 
     public String getNumber() {
@@ -109,6 +141,6 @@ public class BankAccount {
     }
 
     public String getDeposit_type() {
-        return deposit_type;
+        return depositType;
     }
 }
