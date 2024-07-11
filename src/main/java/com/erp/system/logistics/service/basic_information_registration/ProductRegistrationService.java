ca82be11ef84d8a01438e879563b8e94022c91f1
chanminkim-K <snoopy0813@naver.com>
2024-07-12 00:47:56 +0900

    feature: 품목 등록 Service 와 Controller 생성
    - 품목 등록을 위한 Service 코드와 Controller 코드 작성

---
+++ src/main/java/com/erp/system/logistics/service/basic_information_registration/ProductRegistrationService.java
@@ -0,0 +1,7 @@
+package com.erp.system.logistics.service.basic_information_registration;
+
+import com.erp.system.logistics.model.dto.ProductRegistrationDto;
+
+public interface ProductRegistrationService {
+    void registerProduct(ProductRegistrationDto productRegistrationDto);
+}
